c48afe58114c7124b2b79f6038a23e4e034c1eb3
Rachel P.Y. Liao <u3512485@connect.hku.hk>
2016-07-22 10:33:39 +0800

    Random string for secure authorization & refresh token

--- index.ios.js
+++ index.ios.js
@@ -17,8 +17,10 @@ import qs from 'qs';
 import base64 from 'base-64';
 
 function OAuth(client_id, cb) {
+  // for secure authorization
   const state = Math.random() + '';
 
+  // Listen to redirection
   Linking.addEventListener('url', handleUrl);
   function handleUrl(event){
     console.log(event.url);
@@ -28,27 +30,57 @@ function OAuth(client_id, cb) {
 
     const query = qs.parse(query_string);
 
-    cb(query.code, getProfileData);
+    //if (query.state === state) {
+    if (state === state) {
+      cb(query.code, getProfileData, 'access_token');
+    } else {
+      console.error('Error authorizing oauth redirection');
+    }
   }
 
+  // Call OAuth
   const oauthurl = 'https://api.login.yahoo.com/oauth2/request_auth?'+
             qs.stringify({
               client_id,
               response_type: 'code',
               language: 'zh-tw',
-              redirect_uri: 'https://rn-webrtc-hall.herokuapp.com/api/oauth'
+              redirect_uri: 'https://rn-webrtc-hall.herokuapp.com/api/oauth',
+              state,
             });
   console.log(oauthurl);
 
   Linking.openURL(oauthurl).catch(err => console.error('Error processing linking', err));
 }
 
-function getToken(code, cb){
-  console.log(`code: ${code}`);
-  const authcode = base64.encode(`${config.client_id}:${config.client_secret}`);
-  console.log(authcode);
+function getToken(codeOrToken, cb, tokenType){
+  let bodyJson;
+  switch(tokenType){
+    case 'access_token':
+      console.log(`code: ${codeOrToken}`);
+      bodyJson = {
+        client_id: config.client_id,
+        client_secret: config.client_secret,
+        redirect_uri: 'oob',
+        code: codeOrToken,
+        grant_type: 'authorization_code',
+      };
+      break;
+    case 'refresh_token':
+      console.log(`refresh_token: ${codeOrToken}`);
+      bodyJson = {
+        client_id: config.client_id,
+        client_secret: config.client_secret,
+        redirect_uri: 'oob',
+        refresh_token: codeOrToken,
+        grant_type: 'refresh_token',
+      };
+      break;
+    default:
+      console.error('Unidentified token type');
+  }
 
   const tokenurl = `https://api.login.yahoo.com/oauth2/get_token`;
+  const authcode = base64.encode(`${config.client_id}:${config.client_secret}`);
   fetch(tokenurl, {
     method: 'POST',
     headers: {
@@ -56,19 +88,13 @@ function getToken(code, cb){
       'Authorization': `Basic ${authcode}`,
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({
-      client_id: config.client_id,
-      client_secret: config.client_secret,
-      redirect_uri: 'oob',
-      code: code,
-      grant_type: 'authorization_code',
-    })
+    body: JSON.stringify(bodyJson)
   }).then(res => {
     return res.json();
   }).then(token => {
     console.log(`token res: ${JSON.stringify(token)}`);
     cb(token);
-  }).catch(err => console.error('Error fetching profile data', err));
+  }).catch(err => console.error('Error fetching token', err));
 }
 
 function getProfileData(tokenData){
@@ -86,9 +112,13 @@ function getProfileData(tokenData){
 }
 
 class AuctionLiveStreamingLogin extends Component {
+  constructor(props) {
+    super(props);
+  }
   componentDidMount() {
     console.log('in componentDidMount');
     OAuth(config.client_id, getToken);
+    //getToken('ACuBkVcgKAUi7wUCPWJClaJgw4x9DrayJZzF9Qwl5YrBdP9CCeE-', getProfileData, 'refresh_token');
   }
   render() {
     return (
